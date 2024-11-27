b35bbce4c079c4b85ebd169b8353a2819d723837
Globhs <Kasperh03@gmail.com>
2024-11-27 15:43:27 +0100

    added a loginmenu

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using System.Collections.Generic;
+
+namespace app;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.loginMenu();
+    }
+    public void loginMenu()
+    {
+        Console.WriteLine("Login with Username and Password!");
+    
+        string correctUsername = "Kasper"; //Här får vi callea på admin usernames samt admin passwords
+        string correctPassword = "Kasper123";
+    
+        bool isLoggedIn = false; 
+    
+        while (!isLoggedIn)
+        {
+            Console.WriteLine("Enter your username: ");
+            String username = Console.ReadLine();
+    
+            Console.WriteLine("Enter your password ");
+            String password = Console.ReadLine();
+    
+            if (correctLogin(username, password, correctUsername, correctPassword)) // ifall denna boolean är sann skriv ut att man är utloggad och gå vidare till general menu. pst inte implementerat general menu.
+            {
+                Console.WriteLine("\nCorrect credentials! You are now logged in!");
+                isLoggedIn = true;
+            }
+            else
+            {
+                Console.WriteLine("\nIncorrect credentials! Try again!");
+            }
+            
+        }
+    
+        bool correctLogin(string username, string password, string correctUsername, string correctPassword)
+        {
+            return username == correctUsername && password == correctPassword;
+        }
+    
+    }
+}
+
+
+
+
