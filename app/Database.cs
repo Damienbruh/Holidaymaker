4c0acb3cc6e611b53c2ab88d739380b6d4cfbc50
David Hansén <31345425+davezargar@users.noreply.github.com.>
2024-11-28 16:15:21 +0100

    added .env to prevent my silly mistakes again :( no hack me pls i used it for roblox

--- app/Database.cs
+++ app/Database.cs
@@ -1,14 +1,10 @@
+using DotNetEnv;
 using Npgsql;
 
 namespace app
 {
     public class Database
     {
-        private readonly string _host = "localhost";
-        private readonly string _port = "5432";
-        private readonly string _username = "postgres";
-        private readonly string _password = "emtpy";
-        private readonly string _database = "holiday_maker";
 
         private NpgsqlDataSource _connection;
 
@@ -19,7 +15,7 @@ namespace app
 
         public Database()
         {
-            _connection = NpgsqlDataSource.Create($"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database};SearchPath=public");
+            _connection = NpgsqlDataSource.Create(Env.GetString("connectString"));
             using var conn = _connection.OpenConnection();
         }
 
--- app/Program.cs
+++ app/Program.cs
@@ -1,4 +1,5 @@
 ﻿using app.Queries;
+using DotNetEnv;
 namespace app;
 
 
@@ -6,13 +7,15 @@ class Program
 {
     static void Main()
     {
+        Env.TraversePath().Load();
+
         Database db = new();
         TestQueries queries = new(db.Connection());
         //HotellQueries hotellQueries = new(db.Connection());
         QueryHandler queryHandler = new(db.Connection());
-        Menu menu = new(queryHandler);
+        //Menu menu = new(queryHandler);
         
-        //queries.AllCustomers();
+        queries.AllCustomers();
         
     }
 }
