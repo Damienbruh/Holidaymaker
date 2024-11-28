83b24765eb26f343f7ce87b326eb1798d5e8c65e
David Hansén <31345425+davezargar@users.noreply.github.com.>
2024-11-28 14:56:13 +0100

    small change

--- app/Menu.cs
+++ app/Menu.cs
@@ -37,6 +37,7 @@ public class Menu
                 }
                 break;
             case MenuStateEnum.Main:
+                
                 break;
             case MenuStateEnum.ViewBookings:
                 break;
