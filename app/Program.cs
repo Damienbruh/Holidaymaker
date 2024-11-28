f56e652ea12b086e43642b43735942fa631d4a5c
Damien <damients@gmail.com>
2024-11-28 14:39:09 +0100

    I messed up

--- app/Program.cs
+++ app/Program.cs
@@ -5,29 +5,7 @@ namespace app;
 
 class Program
 {
-    static async Task Main(string[] args)
-    {
-        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=1234;Database=postgres";
-
-        var customerQuery = new CustomerQuery(connectionString);
-
-        try
-        {
-            var customers = await customerQuery.GetCustomersAsync();
-            foreach (var customer in customers)
-            {
-                Console.WriteLine($"ID: {customer.CustomerId}, Name: {customer.Name}, Email: {customer.Email}, " +
-                                  $"Phone: {customer.PhoneNumber}, Birth Year: {customer.BirthYear}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-    } 
-}
-    
-   /* static void Main()
+    static void Main()
     {
         Database db = new();
         TestQueries queries = new(db.Connection());
@@ -38,7 +16,7 @@ class Program
         
     }
 }
-*/
+
 
 
     
