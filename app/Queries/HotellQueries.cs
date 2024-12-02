09c4aa6db3ec6479e4e7237da6c80a8a035749b8
Globhs <Kasperh03@gmail.com>
2024-12-02 14:00:04 +0100

    {     foreach (var entry in columnMappings)     {         int columnIndex = entry.Key;        // Column index         List<string> targetList = entry.Value; // Corresponding list
            // Add data or "null" based on IsDBNull
            if (reader.IsDBNull(columnIndex))
            {
                targetList.Add("null");
            }
            else
            {
                targetList.Add(reader.GetValue(columnIndex).ToString());
            }
        }
    }

--- app/Queries/HotellQueries.cs
+++ app/Queries/HotellQueries.cs
@@ -1,18 +1,31 @@
-﻿using Npgsql;
+﻿using System.Data.Common;
+using Npgsql;
 namespace app.Queries;
 
 public class HotellQueries
 {
     private NpgsqlDataSource _database;
     
+    List<string?> _streetNames;
     List<string?> _postalCodes;
+    List<string?> _cities;
     List<string?> _region;
+    List<string?> _country;
+    List<string?> _distancesToSkiSlope;
+    List<string?> _distancesToTownCenter;
+    List<string?> _ratings;
 
     public HotellQueries(NpgsqlDataSource database)
     {
         _database = database;
+        _streetNames = new List<string?>();   
         _postalCodes = new List<string?>();
+        _cities = new List<string?>();
         _region = new List<string?>();
+        _country = new List<string?>();
+        _distancesToSkiSlope = new List<string?>();
+        _distancesToTownCenter = new List<string?>();
+        _ratings = new List<string?>();
     }
 
     public async void SpecificHotell()
@@ -23,13 +36,41 @@ public class HotellQueries
                 await using (var reader = await cmd.ExecuteReaderAsync()) // Kör vår kommando/query och inväntar resultatet.
                     while ( await reader.ReadAsync()) // Läser av 1 rad/objekt i taget ifrån resultatet och kommer avsluta loopen när det inte finns fler rader att läsa. 
                     {
-                        if (reader.IsDBNull(2))
+                        Dictionary<int, List<string>> columnMappings = new Dictionary<int, List<string>>()
                         {
+                            { 1, _streetNames },
+                            { 2, _postalCodes },
+                            { 3, _cities },
+                            { 4, _region },
+                            { 5, _country },
+                            { 6, _distancesToSkiSlope },
+                            { 7, _distancesToTownCenter },
+                            { 8, _ratings }
+                        };
+                        while (reader.Read())
+                        {
+                            foreach (var entry in columnMappings)
+                            {
+                                int columnIndex = entry.Key;
+                                List<string> targetList = entry.Value;
+                                
+                                if (reader.IsDBNull(columnIndex))
+                                {
+                                    targetList.Add("null");
+                                }
+                                else
+                                {
+                                    targetList.Add(reader.GetValue(columnIndex).ToString());
+                                }
+                            }
+                        }
+                        /*if (reader.IsDBNull(2))
+                        {   
                             _postalCodes.Add("null");
                         }
                         else
                         {
-                           _postalCodes.Add(reader.GetString(2)); 
+                           _postalCodes.Add(reader.GetString(2));
                         }
                         if (reader.IsDBNull(4))
                         {
@@ -37,29 +78,14 @@ public class HotellQueries
                         }
                         else
                         {
-                            _region.Add(reader.GetString(4)); 
-                        }
-                        /*Console.WriteLine($"hotel_id: {reader.GetInt32(0)}," +
-                                          $"street_name: {reader.GetString(1)}," +
-                                          $"postal_code: {reader.GetString(2)}," +
-                                          $"city: {reader.GetString(3)}," +
-                                          $"region: {reader.GetInt32(4)}" +
-                                          $"country: {reader.GetString(3)}," +
-                                          $"distance_to_ski_slope: {reader.GetInt32(3)},");*/
+                            _region.Add(reader.GetString(4));
+                        }*/
                     }
 
             _postalCodes.ForEach(delegate(string postalcode)
             {
                 Console.WriteLine(postalcode);
             });
-            /*foreach (var VARIABLE in _postalCodes)
-            {
-                Console.WriteLine(VARIABLE);
-                /*if (VARIABLE == null){
-                    Console.WriteLine(VARIABLE);
-                }#1#
-
-            }*/
         }
         catch (Exception e)
         {
