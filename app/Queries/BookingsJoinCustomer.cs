121d1c7cfe0cc047d1782e09822fa8f6724ac947
Damien <damienthuresson@gmail.com>
2024-12-03 17:04:04 +0100

    Added BookingJoinCustomer, should be working

--- app/Queries/BookingsJoinCustomer.cs
+++ app/Queries/BookingsJoinCustomer.cs
@@ -1,6 +1,37 @@
-﻿namespace app.Queries;
+﻿using Npgsql;
+namespace app.Queries;
 
 public class BookingsJoinCustomer
 {
     
+    private NpgsqlDataSource _database;
+
+    public BookingsJoinCustomer(NpgsqlDataSource database)
+    {
+        _database = database;
+    }
+    
+    
+    public async Task<int> InsertBookingsJoinCustomer(int bookingId, List<int> customerIds)
+    {
+        var rowsAffected = 0;
+        foreach (var customerId in customerIds)
+        {
+            var query = @"INSERT INTO bookings_join_customer (rooms_fk, customer_fk)
+                      VALUES ($1, $2)";
+            await using (var cmd = _database.CreateCommand(query))
+            {
+                cmd.Parameters.AddWithValue(bookingId);
+                cmd.Parameters.AddWithValue(customerId);
+                
+                await cmd.ExecuteNonQueryAsync();
+                rowsAffected++; 
+            }
+        }
+        
+        Console.WriteLine(rowsAffected);
+        Console.ReadLine();
+        return rowsAffected;
+
+    } 
 }
